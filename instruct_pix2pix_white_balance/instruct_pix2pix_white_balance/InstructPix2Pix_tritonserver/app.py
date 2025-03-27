4596c53bfeb7b83372fdad213ffb83223bbd68f9
ThanhHoa_JIKAY <hoanguyenthanh07@gmail.com>
2025-03-28 00:13:16 +0700

    update_app
    update_app

--- instruct_pix2pix_white_balance/instruct_pix2pix_white_balance/InstructPix2Pix_tritonserver/app.py
+++ instruct_pix2pix_white_balance/instruct_pix2pix_white_balance/InstructPix2Pix_tritonserver/app.py
@@ -13,7 +13,7 @@ from triton_clients.vae_decoder_client import decode_vae_latents
 
 app = FastAPI()
 
-model_id = "timbrooks/instruct-pix2pix"
+model_id = "hoanguyenthanh07/instruct-pix2pix"
 pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(
     model_id, torch_dtype=torch.float16, safety_checker=None
 )
